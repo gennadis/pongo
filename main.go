b66a7dd491acf7d9a418f3e12c071d1b9743c3bb
Gennady Badinov <gennady.badinov@gmail.com>
2025-12-27 20:24:13 +0300

    feat: add basic animation

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"os"
+	"sync"
+	"time"
 
 	"github.com/gdamore/tcell/v2"
 )
@@ -20,13 +22,32 @@ func main() {
 	style := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorWhite)
 	screen.SetStyle(style)
 
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go runGameLoop(&wg, screen, style)
+	go handleKeyPress(&wg, screen)
+	wg.Wait()
+}
+
+func runGameLoop(wg *sync.WaitGroup, screen tcell.Screen, style tcell.Style) {
+	defer wg.Done()
+	x := 0
 	for {
-		screen.SetContent(0, 0, 'h', nil, style)
-		screen.SetContent(1, 0, 'e', nil, style)
-		screen.SetContent(2, 0, 'l', nil, style)
-		screen.SetContent(3, 0, 'l', nil, style)
-		screen.SetContent(4, 0, 'o', nil, style)
+		screen.Clear()
+		screen.SetContent(x, 10, 'h', nil, style)
+		screen.SetContent(x+1, 10, 'e', nil, style)
+		screen.SetContent(x+2, 10, 'l', nil, style)
+		screen.SetContent(x+3, 10, 'l', nil, style)
+		screen.SetContent(x+4, 10, 'o', nil, style)
 		screen.Show()
+		x++ // move text right
+		time.Sleep(40 * time.Millisecond)
+	}
+}
+
+func handleKeyPress(wg *sync.WaitGroup, screen tcell.Screen) {
+	defer wg.Done()
+	for {
 		switch event := screen.PollEvent().(type) {
 		case *tcell.EventResize:
 			screen.Sync()
