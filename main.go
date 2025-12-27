b5103c21104fd65cd3b204e3110ab7cf1b639218
Gennady Badinov <gennady.badinov@gmail.com>
2025-12-27 19:30:15 +0300

    feat: init screen

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gdamore/tcell/v2"
+)
 
 func main() {
-	fmt.Println("hello world")
+	screen, err := tcell.NewScreen()
+	if err != nil {
+		log.Fatalf("new screen: %+v", err)
+	}
+
+	if err := screen.Init(); err != nil {
+		log.Fatalf("screen init: %+v", err)
+	}
+
+	style := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorBlack)
+	screen.SetStyle(style)
+
+	for {
+		screen.SetContent(0, 0, 'h', nil, style)
+		screen.SetContent(1, 0, 'e', nil, style)
+		screen.SetContent(2, 0, 'l', nil, style)
+		screen.SetContent(3, 0, 'l', nil, style)
+		screen.SetContent(4, 0, 'o', nil, style)
+		screen.Show()
+	}
 }
